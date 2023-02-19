5e0c247d407a7bef9660be46c75d9d7cb0079600
negiizhao <egresf@gmail.com>
2023-02-20 03:11:34 +0800

    some Graph APIs & some AssocArray APIs

--- Algorithm/Data/Classes/AssocArray.lean
+++ Algorithm/Data/Classes/AssocArray.lean
@@ -3,7 +3,8 @@ Copyright (c) 2023 Yuyang Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
-import Mathlib.Data.Fin.Basic
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Setoid.Basic
 import Std.Data.Array.Lemmas
 
 /-- `AssocArray ι α M` is a data structure that acts like a finitely supported function `ι → α`
@@ -36,8 +37,81 @@ instance [Inhabited α] : AssocArray (Fin n) α {a : Array α // a.size = n} whe
 end Array
 
 namespace AssocArray
+
+class Ext (ι : outParam <| Type _) [DecidableEq ι]
+    (α : outParam <| Type _) [Inhabited α] (M : Type _) [AssocArray ι α M] where
+  ext : ∀ {m₁ m₂ : M}, get m₁ = get m₂ → m₁ = m₂
+
 variable {ι : Type _} [DecidableEq ι] {α : Type _} [Inhabited α] {M : Type _} [AssocArray ι α M]
 
--- instance instGetElem : GetElem M ι α (fun _ _ ↦ True) := ⟨fun m i _ ↦ AssocArray.get m i⟩
+variable (M)
+
+def Quotient := @_root_.Quotient M (Setoid.ker get)
+
+instance : AssocArray ι α (Quotient M) where
+  empty := ⟦empty⟧
+  set q i v := q.map' (set · i v) (fun _ _ hm ↦ (Eq.congr get_set get_set).mpr (by rw [hm]))
+  get := Quotient.lift get (fun _ _ ↦ id)
+  get_set {q i v} := q.inductionOn (fun _ ↦ get_set)
+  get_empty := get_empty
+
+instance : Ext ι α (Quotient M) where
+  ext {m₁ m₂} := m₂.inductionOn <| m₁.inductionOn (fun _ _ ha ↦ Quotient.sound ha)
+export Ext (ext)
+
+def listIndicator (l : List ι) (f : ∀ i ∈ l, α) : M :=
+  match l with
+  | [] => empty
+  | (i :: l) => set (listIndicator l (fun i hi ↦ f i (List.mem_cons_of_mem _ hi)))
+    i (f i (List.mem_cons_self _ _))
+
+variable {M}
+
+lemma get_listIndicator (l : List ι) (f : ∀ i ∈ l, α) :
+    get (listIndicator M l f) = (fun i ↦ if hi : i ∈ l then f i hi else default) :=
+  match l with
+  | [] => by simp [listIndicator, get_empty, Function.const]
+  | (i :: l) => by
+    ext j
+    rw [listIndicator, get_set, Function.update_apply]
+    split_ifs with h₁ h₂
+    · simp [h₁]
+    · simp [h₁] at h₂
+    · rename_i h₂ -- a `split_ifs` bug
+      simp_rw [get_listIndicator, dif_pos (List.mem_of_ne_of_mem h₁ h₂)]
+    · rename_i h₂ -- a `split_ifs` bug
+      simp_rw [get_listIndicator, dif_neg (List.not_mem_of_not_mem_cons h₂)]
+
+variable [Ext ι α M]
+variable (M)
+
+def indicator (s : Finset ι) (f : ∀ i ∈ s, α) : M :=
+  let this := Equiv.subtypeQuotientEquivQuotientSubtype (fun l : List ι ↦ ↑l = s.1)
+    (fun m ↦ m = s.1) (fun i ↦ Iff.rfl) (fun _ _ ↦ Iff.rfl) ⟨s.1, rfl⟩
+  this.liftOn (fun l ↦ listIndicator M l (fun i hi ↦ f i (l.2 ▸ hi : i ∈ s.1)))
+    (fun l₁ l₂ hl ↦ ext <| by simp_rw [get_listIndicator, List.Perm.mem_iff hl])
+
+variable {M}
+
+lemma get_indicator (s : Finset ι) (f : ∀ i ∈ s, α) :
+    get (indicator M s f) = (fun i ↦ if hi : i ∈ s then f i hi else default) := by
+  unfold indicator
+  change _ = (fun i ↦ if hi : i ∈ s.1 then _ else _)
+  obtain ⟨l, hl⟩ := s.1.exists_rep
+  simp_rw [← hl]
+  rw [Equiv.subtypeQuotientEquivQuotientSubtype_mk]
+  dsimp
+  rw [get_listIndicator]
+  rfl
+
+variable (M)
+
+def ofFun [Fintype ι] (f : ι → α) : M := indicator M Finset.univ (fun i _ ↦ f i)
+
+variable {M}
+
+lemma get_ofFun [Fintype ι] (f : ι → α) :
+    get (ofFun M f) = f :=
+  (get_indicator _ _).trans <| funext fun _ ↦ dif_pos <| Finset.mem_univ _
 
 end AssocArray
--- Algorithm/Data/Graph.lean
+++ Algorithm/Data/Graph.lean
@@ -1,34 +1,53 @@
 
 import Algorithm.Data.Classes.AssocArray
+import Mathlib.Data.List.Nodup
 
 structure Graph where
   V : Type _
-  [instDecidableEqV : DecidableEq V]
-  E : Type _
-  fst' : E → V
-  snd' : E → V
+  [decidableEqV : DecidableEq V]
+  EType : Type _
+  fst' : EType → V
+  snd' : EType → V
   StarList : Type _
-  [instAssocArrayStarList : AssocArray V (List E) StarList]
+  [assocArrayStarList : AssocArray V (List EType) StarList]
   star' : StarList
   costar' : StarList
   fst_star' : ∀ v, ∀ e ∈ AssocArray.get star' v, fst' e = v
   snd_costar' : ∀ v, ∀ e ∈ AssocArray.get costar' v, snd' e = v
+  nodup_star' : ∀ v, (AssocArray.get star' v).Nodup
+  nodup_costar' : ∀ v, (AssocArray.get costar' v).Nodup
+  mem_star'_iff_mem_costar' : ∀ e,
+    e ∈ AssocArray.get star' (fst' e) ↔ e ∈ AssocArray.get costar' (snd' e)
 
 namespace Graph
-variable {g : Graph}
 
-instance : DecidableEq g.V := g.instDecidableEqV
-instance : AssocArray g.V (List g.E) g.StarList := g.instAssocArrayStarList
+attribute [instance] decidableEqV assocArrayStarList
+
+variable {g : Graph}
 -- instance : GetElem g.StarList g.V (List g.E) (fun _ _ ↦ True) := AssocArray.instGetElem
 -- by infer_instance
 
-def E.fst (e : g.E) : g.V := g.fst' e
-def E.snd (e : g.E) : g.V := g.snd' e
+def EType.fst (e : g.EType) : g.V := g.fst' e
+def EType.snd (e : g.EType) : g.V := g.snd' e
 
-def V.star (v : g.V) : List g.E := AssocArray.get g.star' v
-def V.costar (v : g.V) : List g.E := AssocArray.get g.costar' v
+def V.star (v : g.V) : List g.EType := AssocArray.get g.star' v
+def V.costar (v : g.V) : List g.EType := AssocArray.get g.costar' v
 
 lemma fst_star (v : g.V) : ∀ e ∈ v.star, e.fst = v := g.fst_star' v
 lemma snd_costar (v : g.V) : ∀ e ∈ v.costar, e.snd = v := g.snd_costar' v
+
+lemma nodup_star (v : g.V) : v.star.Nodup := g.nodup_star' v
+lemma nodup_costar (v : g.V) : v.costar.Nodup := g.nodup_costar' v
+
+lemma mem_star_iff_mem_costar (e : g.EType) : e ∈ e.fst.star ↔ e ∈ e.snd.costar :=
+  g.mem_star'_iff_mem_costar' e
+
+def E (g : Graph) := {e : g.EType // e ∈ e.fst.star}
+
+def E.fst (e : g.E) : g.V := e.val.fst
+def E.snd (e : g.E) : g.V := e.val.snd
+
+lemma E.mem_star (e : g.E) : e.val ∈ e.fst.star := e.2
+lemma E.mem_costar (e : g.E) : e.val ∈ e.snd.costar := (mem_star_iff_mem_costar _).mp e.2
 
 end Graph
