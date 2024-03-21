14c5aa298dff6788ac9ea2d545a2b6a5110bfea5
negiizhao <egresf@gmail.com>
2024-03-21 16:48:59 +0800

    Update DFS.lean

--- Algorithm/Graph/DFS.lean
+++ Algorithm/Graph/DFS.lean
@@ -54,3 +54,72 @@ lemma dfs_lemma₁ {BoolArray : Type*} [AssocArray BoolArray V Bool]
         simp only [E.snd_val, ofHom_snd, toQuiver_ofQuiver]
         exact .nil
     · exact .inr ⟨u, hu, ⟨p.cons e⟩⟩
+
+lemma dfs_iff [Fintype V] {BoolArray : Type*} [AssocArray BoolArray V Bool]
+    {g : AdjList V EType EColl StarList} {vs : List V} {visited : BoolArray}
+    (h : ∀ v : V, visited[v] → ∀ e ∈ g.star[v], visited[g.snd e] ∨ g.snd e ∈ vs) (v : V) :
+    ((g.dfs vs visited)[v] ↔
+      visited[v] ∨ ∃ u ∈ vs, Nonempty (Quiver.Path (g.toQuiver u) (g.toQuiver v))) :=
+  match vs with
+  | [] => by simp [dfs]
+  | u :: vs => by
+    simp only [dfs, List.mem_cons, exists_eq_or_imp]
+    split_ifs with hv
+    · have (v : V) (hv : visited[v] = true) (e) (he : e ∈ g.star[v]) :
+          visited[g.snd e] = true ∨ g.snd e ∈ vs
+      · simp only [List.mem_cons] at h
+        obtain (h | rfl | h) := h v hv e he
+        exacts [.inl h, .inl hv, .inr h]
+      rw [dfs_iff this]
+      constructor
+      · rintro (h | h)
+        exacts [.inl h, .inr <| .inr h]
+      · rintro (h | ⟨⟨p⟩⟩ | h)
+        · exact .inl h
+        · exact dfs_lemma₁ this hv p
+        · exact .inr h
+    · rw [dfs_iff]
+      simp only [AssocArray.getElem_update, Function.update, List.mem_append, List.mem_map,
+        or_and_right, exists_or]
+      split_ifs with hvu
+      · subst hvu
+        have : Nonempty (Quiver.Path (g.toQuiver v) (g.toQuiver v)) := ⟨.nil⟩
+        simp [this]
+      · refine or_congr .rfl (or_congr ⟨?_, ?_⟩ .rfl)
+        · rintro ⟨_, ⟨e, he, rfl⟩, ⟨p⟩⟩
+          have := g.fst_star _ _ he
+          subst this
+          exact ⟨Quiver.Path.comp (Quiver.Path.nil.cons (g.toHom ⟨e, he⟩)) p⟩
+        · rintro ⟨p⟩
+          match p with
+          | nil => exact absurd rfl hvu
+          | cons .nil e =>
+            exact
+
+          | cons p e =>
+            exact
+            simp?
+
+
+
+
+
+
+lemma dfs_iff [Fintype V] {BoolArray : Type*} [AssocArray BoolArray V Bool]
+    {g : AdjList V EType EColl StarList} {vs : List V} {visited : BoolArray}
+    (h : ∀ v : V, visited[v] → ∀ e ∈ g.star[v], visited[g.snd e] ∨ g.snd e ∈ vs) (v : V) :
+    ((g.dfs vs visited)[v] ↔
+      visited[v] ∨ ∃ u ∈ vs, Nonempty (Quiver.Path (g.toQuiver u) (g.toQuiver v))) :=
+  match vs with
+  | [] => by simp [dfs]
+  | u :: vs => by
+    simp [dfs]
+    split_ifs with hv
+    · rw [dfs_iff]
+      apply or_congr Iff.rfl
+
+
+
+
+
+end AdjList
