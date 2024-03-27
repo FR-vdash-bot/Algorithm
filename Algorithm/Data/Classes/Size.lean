e30f6e4dde1315f50d2924c80ea935aedc9e350f
negiizhao <egresf@gmail.com>
2024-03-27 12:57:47 +0800

    name
    name

---
+++ Algorithm/Data/Classes/Size.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2023 Yuyang Zhao. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yuyang Zhao
+-/
+
+class Size (C : Type _) (α : outParam <| Type _) where
+  size : C → Nat
+  isEmpty : C → Bool
+  isEmpty_iff_size_eq_zero c : isEmpty c ↔ size c = 0
+export Size (size isEmpty isEmpty_iff_size_eq_zero)
--- Algorithm/Data/Classes/ToMultiset.lean
+++ Algorithm/Data/Classes/ToMultiset.lean
@@ -3,41 +3,35 @@ Copyright (c) 2023 Yuyang Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
+import Algorithm.Data.Classes.Size
 import Mathlib.Data.Multiset.Basic
 
-class CollectionCore (C : Type*) (α : outParam Type*) where
-  size : C → Nat
-  isEmpty : C → Bool
-  isEmpty_iff_size_eq_zero c : isEmpty c ↔ size c = 0
-export CollectionCore (size isEmpty isEmpty_iff_size_eq_zero)
-
--- do not need to write `toMultiset` if we have `toList`
-class Collection (C : Type*) (α : outParam Type*) extends CollectionCore C α where
+class ToMultiset (C : Type*) (α : outParam Type*) extends Size C α where
   toMultiset : C → Multiset α
   card_toMultiset_eq_size c : Multiset.card (toMultiset c) = size c
-export Collection (toMultiset card_toMultiset_eq_size)
+export ToMultiset (toMultiset card_toMultiset_eq_size)
 
-attribute [simp] Collection.card_toMultiset_eq_size
+attribute [simp] ToMultiset.card_toMultiset_eq_size
 
 section
 variable {α : Type*}
 
-instance : Collection (List α) α where
+instance : ToMultiset (List α) α where
   toMultiset := (↑)
   size := List.length
   card_toMultiset_eq_size c := rfl
   isEmpty := List.isEmpty
   isEmpty_iff_size_eq_zero l := by cases l <;> simp
 
-instance : Collection (Array α) α where
+instance : ToMultiset (Array α) α where
   toMultiset a := ↑a.toList
   size := Array.size
   card_toMultiset_eq_size c := by simp
   isEmpty := Array.isEmpty
   isEmpty_iff_size_eq_zero l := by simp
 
-section Collection
-variable {C α : Type*} [Collection C α] (c : C)
+section ToMultiset
+variable {C α : Type*} [ToMultiset C α] (c : C)
 
 instance (priority := 100) : Membership α C where
   mem a c := a ∈ toMultiset c
@@ -55,7 +49,7 @@ lemma isEmpty_eq_size_beq_zero : isEmpty c = (size c == 0) := by
 variable [DecidableEq α]
 
 def countSlow (a : α) (c : C) : Nat :=
-  (Collection.toMultiset c).count a
+  (ToMultiset.toMultiset c).count a
 
 @[simp]
 theorem countSlow_eq_zero {a : α} {c : C} : countSlow a c = 0 ↔ a ∉ c :=
@@ -64,10 +58,10 @@ theorem countSlow_eq_zero {a : α} {c : C} : countSlow a c = 0 ↔ a ∉ c :=
 theorem countSlow_ne_zero {a : α} {c : C} : countSlow a c ≠ 0 ↔ a ∈ c := by
   simp [Ne.def, countSlow_eq_zero]
 
-class Count (C : Type*) (α : outParam Type*) [Collection C α] where
+class Count (C : Type*) (α : outParam Type*) [ToMultiset C α] where
   count : [DecidableEq α] → α → C → Nat
   count_eq_countSlow : [DecidableEq α] → ∀ a c,
     count a c = countSlow a c := by intros; rfl
 export Count (count count_eq_countSlow)
 
-end Collection
+end ToMultiset
