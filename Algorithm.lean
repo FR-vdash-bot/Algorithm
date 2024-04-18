a5433e8a76366793e74738038fd1f4ca49862f33
negiizhao <zhao.yu-yang@foxmail.com>
2024-04-19 04:23:21 +0800

    oops

--- Algorithm.lean
+++ Algorithm.lean
@@ -1,12 +1,9 @@
 import Algorithm.Algebra.BigOperators.DFinsupp'
 import Algorithm.Data.Classes.AssocArray
-import Algorithm.Data.Classes.Collection
 import Algorithm.Data.Classes.Bag
 import Algorithm.Data.Classes.Container
 import Algorithm.Data.Classes.DynamicArrayLike
 import Algorithm.Data.Classes.IndexedMinHeap
-import Algorithm.Data.Classes.StackLike
-import Algorithm.Data.Graph
 import Algorithm.Data.Classes.MultiBag
 import Algorithm.Data.Classes.Size
 import Algorithm.Data.Classes.ToFinset
