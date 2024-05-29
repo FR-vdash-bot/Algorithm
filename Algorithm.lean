c3e614321905ca23c46cb43c245bd4007bd39a99
FR <zhao.yu-yang@foxmail.com>
2024-05-29 19:47:04 +0800

    chore: move files (#24)

--- Algorithm.lean
+++ Algorithm.lean
@@ -14,6 +14,7 @@ import Algorithm.Data.DFinsupp'.Fintype
 import Algorithm.Data.Forest
 import Algorithm.Data.Graph.AdjList
 import Algorithm.Data.Graph.IsDFSForest
+import Algorithm.Data.MutableQuotient
 import Algorithm.Data.PairingHeap
 import Algorithm.Data.UnionFind
 import Algorithm.Graph.DFS
