db8d8e90d66e7b9f570cfa814a37f996134fc95c
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-03-19 02:11:34 +0800

    chore: move to v4.17.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -2,8 +2,8 @@ import Lake
 
 open Lake DSL
 
-require "leanprover-community" / "mathlib" @ git "v4.16.0"
-require "leanprover" / "doc-gen4" @ git "v4.16.0"
+require "leanprover-community" / "mathlib" @ git "v4.17.0-rc1"
+require "leanprover" / "doc-gen4" @ git "v4.17.0-rc1"
 
 abbrev algorithmOnlyLinters : Array LeanOption := #[
   ⟨`linter.hashCommand, true⟩,
