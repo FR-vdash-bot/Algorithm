4c030b38447960b64f0897011e882fcb2eb6af18
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2025-08-02 10:11:59 +0800

    Update Dict.lean

--- Algorithm/Data/Classes/Dict.lean
+++ Algorithm/Data/Classes/Dict.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Yuyang Zhao. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yuyang Zhao
 -/
-import Algorithm.Data.Classes.DefaultDict
 import Algorithm.Data.Classes.Bag
+import Algorithm.Data.Classes.DefaultDict
 import Algorithm.Data.Classes.GetElem
 
 variable {C ι α : Type*}
